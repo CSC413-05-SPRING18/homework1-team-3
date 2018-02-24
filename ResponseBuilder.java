fb9ed9769bf9c3e74d4ab43e0825397c8419c4aa
Quan Lu <35711859+Quanlu413@users.noreply.github.com>
2018-02-24 07:23:21 -0800

    Add files via upload

---
+++ ResponseBuilder.java
@@ -0,0 +1,40 @@
+package simpleserver;
+
+public class ResponseBuilder {
+
+  private Response response;
+  private Data[] data;
+  private StatusCode statusCode;
+
+  public enum StatusCode {
+    OK,
+    ERROR_GENERAL,
+  }
+
+
+  public void setStatus(StatusCode statusCode) {
+    this.statusCode = statusCode;
+  }
+
+  public Response build() {
+    String statusString;
+    switch (this.statusCode) {
+      case OK:
+        statusString = "OK";
+        break;
+      default:
+        statusString = "ERROR_GENERAL";
+    }
+    if (data == null){
+      data = new Data[0];
+    }
+    return new Response(statusString, data.length, data);
+  }
+
+  public static void main(String args[]){
+    ResponseBuilder responseBuilder = new ResponseBuilder();
+    responseBuilder.setStatus(StatusCode.OK);
+    Response response = responseBuilder.build();
+    System.out.println(response.getStatusString());
+  }
+}
