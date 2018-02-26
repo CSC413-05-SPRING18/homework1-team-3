f10744c833bcddfb76c50dc403ad5db81e27b1bd
Quan Lu <35711859+Quanlu413@users.noreply.github.com>
2018-02-26 03:38:53 -0800

    Add files via upload
    Add some change on Data

--- src/simpleserver/Data.java
+++ src/simpleserver/Data.java
@@ -1,6 +1,9 @@
 package simpleserver;
 
-import java.util.*;
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 
 public class Data {
@@ -8,7 +11,7 @@ public class Data {
     private int userid;
     private int postid;
     private String username;
-    private String data;
+
 
     public void setUserID(int new_id)
     {
@@ -31,4 +34,45 @@ public class Data {
     }
 
     //add methods for Post data
-}
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Users: ").append(",");
+        builder.append("Username: ").append(username).append(",");
+        builder.append("Userid: ").append(userid).append(",");
+        return builder.toString();
+    }
+
+    private List<Data> data = null;
+
+    public List<Data> getData() {
+        return data;
+    }
+    public void setData(List<Data> data){
+        this.data = data;
+    }
+
+    public abstract class JsonDeserializerData implements JsonDeserializer<Data> {
+        public Data deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+                JsonObject user = json.getAsJsonObject();
+                String username = user.get("username: ").getAsString();
+                int userid = user.get("userid: ").getAsInt();
+                String data = user.get("data: ").getAsString();
+
+                User users = new User();
+                users.setUsername(username);
+                users.setUserID(userid);
+
+                return users;
+
+
+
+            }
+        }
+
+
+
+
+
+    }
+
